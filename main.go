4772fe22c6634e698f6f7a48255c1c966c10f35e
LucianoAd <luchiadaglio7@gmail.com>
2026-08-30 20:28:31 -0300

    Creo html 404

--- main.go
+++ main.go
@@ -6,6 +6,22 @@ import (
 )
 
 func main() {
+	html404 := `<!DOCTYPE html>
+	<html lang="es">
+	<head>
+    	<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>Página no encontrada</title>
+	</head>
+	<body>
+    	<div class="contenedor">
+        	<h1>Error 404</h1>
+        	<p>La página que estás buscando no existe.</p>
+        	<a href="/">Volver al inicio</a>
+    	</div>
+	</body>
+	</html>`
+
 	// 1. Define el directorio que contiene los archivos estáticos.
 	staticDir := "./static"
 
@@ -24,5 +40,11 @@ func main() {
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Printf("Error al iniciar el servidor: %s\n", err)
+	}
+}
+
+func manejador (w http.ResponseWriter, r *http.Request) https.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 	}
 }

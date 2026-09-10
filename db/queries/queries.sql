91da648b19a079d481c67d07b7c83d360eb203b4
Ian <ianbarletta@gmail.com>
2026-09-10 16:44:36 -0300

    Redefine las operaciones CRUD para queries.sql

--- db/queries/queries.sql
+++ db/queries/queries.sql
@@ -1,21 +1,98 @@
+-- =========================
+-- EQUIPO
+-- =========================
+
 -- name: CreateEquipo :one
-INSERT INTO equipo (id_equipo, nombre_equipo, cant_jugadores, division) 
-VALUES ($1, $2, $3, $4) 
+INSERT INTO equipo (nombre_equipo, division)
+VALUES ($1, $2)
 RETURNING *;
 
 -- name: GetEquipo :one
-SELECT * FROM equipo 
+SELECT *
+FROM equipo
 WHERE id_equipo = $1;
 
 -- name: ListEquipos :many
-SELECT * FROM equipo 
+SELECT *
+FROM equipo
 ORDER BY division ASC, nombre_equipo ASC;
 
 -- name: UpdateEquipo :exec
-UPDATE equipo 
-SET nombre_equipo = $2, cant_jugadores = $3, division = $4 
+UPDATE equipo
+SET nombre_equipo = $2,
+    division = $3
 WHERE id_equipo = $1;
 
 -- name: DeleteEquipo :exec
-DELETE FROM equipo 
+DELETE FROM equipo
 WHERE id_equipo = $1;
+
+
+-- =========================
+-- JUGADOR
+-- =========================
+
+-- name: CreateJugador :one
+INSERT INTO jugador (nombre, apellido, id_equipo)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: GetJugador :one
+SELECT *
+FROM jugador
+WHERE id_jugador = $1;
+
+-- name: ListJugadores :many
+SELECT *
+FROM jugador
+ORDER BY apellido ASC, nombre ASC;
+
+-- name: UpdateJugador :exec
+UPDATE jugador
+SET nombre = $2,
+    apellido = $3,
+    id_equipo = $4
+WHERE id_jugador = $1;
+
+-- name: DeleteJugador :exec
+DELETE FROM jugador
+WHERE id_jugador = $1;
+
+
+-- =========================
+-- PARTIDO
+-- =========================
+
+-- name: CreatePartido :one
+INSERT INTO partido (
+    id_equipo_local,
+    id_equipo_visitante,
+    goles_local,
+    goles_visitante,
+    fecha
+)
+VALUES ($1, $2, $3, $4, $5)
+RETURNING *;
+
+-- name: GetPartido :one
+SELECT *
+FROM partido
+WHERE id_partido = $1;
+
+-- name: ListPartidos :many
+SELECT *
+FROM partido
+ORDER BY fecha ASC;
+
+-- name: UpdatePartido :exec
+UPDATE partido
+SET id_equipo_local = $2,
+    id_equipo_visitante = $3,
+    goles_local = $4,
+    goles_visitante = $5,
+    fecha = $6
+WHERE id_partido = $1;
+
+-- name: DeletePartido :exec
+DELETE FROM partido
+WHERE id_partido = $1;

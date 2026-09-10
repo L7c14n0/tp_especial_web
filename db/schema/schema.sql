f44545c67d983b5e6c9f0726d243ca4028b45688
Ian <ianbarletta@gmail.com>
2026-09-10 16:38:55 -0300

    Redefine database schema modificando equipos y agregando las tablas jugadores y partidos

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -1,6 +1,29 @@
 CREATE TABLE equipo(
     id_equipo SERIAL PRIMARY KEY,
     nombre_equipo VARCHAR(100) NOT NULL,
-    cant_jugadores INT NOT NULL,
-    division VARCHAR(1) NOT NULL
-)
+    division VARCHAR(1) NOT NULL CHECK (division IN ('A', 'B'))
+);
+
+CREATE TABLE jugador (
+    id_jugador SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    id_equipo INT NOT NULL,
+    FOREIGN KEY (id_equipo) REFERENCES equipo(id_equipo)
+);
+
+CREATE TABLE partido (
+    id_partido SERIAL PRIMARY KEY,
+    id_equipo_local INT NOT NULL,
+    id_equipo_visitante INT NOT NULL,
+    goles_local INT NOT NULL,
+    goles_visitante INT NOT NULL,
+    fecha TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (id_equipo_local) REFERENCES equipo(id_equipo),
+    FOREIGN KEY (id_equipo_visitante) REFERENCES equipo(id_equipo),
+
+    CHECK (id_equipo_local <> id_equipo_visitante),
+    CHECK (goles_local >= 0),
+    CHECK (goles_visitante >= 0)
+);
